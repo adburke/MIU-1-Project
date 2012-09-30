d5b3e0840e9d0d7f57cf404569ddc5cde8ba3726
adburke <adburke@fullsail.edu>
2012-09-30 16:45:28 -0500

    JQM work in jqmtest making browse work with json

--- Burke_Aaron_Project_2/App_A/js/jqmtest.js
+++ Burke_Aaron_Project_2/App_A/js/jqmtest.js
@@ -2,81 +2,82 @@
 // the URL passed in. Generate markup for the items in the
 // category, inject it into an embedded page, and then make
 // that page the current active page.
-function showCategory( urlObj, options )
-{
-	var categoryName = urlObj.hash.replace( /.*category=/, "" ),
-
+function showCategory( urlObj, options ) {
+	console.log("got here 3");
+	var categoryName = urlObj.hash.replace( /.*category=/, "" );
+		console.log(categoryName);
 		// Get the object that represents the category we
 		// are interested in. Note, that at this point we could
 		// instead fire off an ajax request to fetch the data, but
 		// for the purposes of this sample, it's already in memory.
-		category = categoryData[ categoryName ],
+		//job = json[x].jobType,
 
 		// The pages we use to display our content are already in
 		// the DOM. The id of the page we are going to write our
 		// content into is specified in the hash before the '?'.
-		pageSelector = urlObj.hash.replace( /\?.*$/, "" );
+		var pageSelector = urlObj.hash.replace( /\?.*$/, "" );
+	if (categoryName){
+			// Get the page we are going to dump our content into.
+			var $page = $( pageSelector ),
 
-	if ( category ) {
-		// Get the page we are going to dump our content into.
-		var $page = $( pageSelector ),
+				// Get the header for the page.
+				$header = $page.children( ":jqmData(role=header)" ),
 
-			// Get the header for the page.
-			$header = $page.children( ":jqmData(role=header)" ),
+				// Get the content area element for the page.
+				$content = $page.children( ":jqmData(role=content)" );
+				console.log(categoryName);
 
-			// Get the content area element for the page.
-			$content = $page.children( ":jqmData(role=content)" ),
+				// The markup we are going to inject into the content
+				// area of the page.
+				var markup = "<p>" + categoryName + " Jobs" + "</p><ul data-role='listview' data-inset='true'>";
+				for(n in json){
+				if ( categoryName === json[n].jobType[1] ) {
+				// The array of items for this category.
+				//cItems = category.items,
 
-			// The markup we are going to inject into the content
-			// area of the page.
-			markup = "<p>" + category.description + "</p><ul data-role='listview' data-inset='true'>",
+				// The number of items in the category.
+				//numItems = cItems.length;
 
-			// The array of items for this category.
-			cItems = category.items,
-
-			// The number of items in the category.
-			numItems = cItems.length;
-
-		// Generate a list item for each item in the category
-		// and add it to our markup.
-		for ( var i = 0; i < numItems; i++ ) {
-			markup += "<li>" + cItems[i].name + "</li>";
+			// Generate a list item f
+				markup += "<li>" + "#: " + json[n].jobNum[1] + "</li>";
+			}
 		}
-		markup += "</ul>";
+			markup += "</ul>";
 
-		// Find the h1 element in our header and inject the name of
-		// the category into it.
-		$header.find( "h1" ).html( category.name );
+			// Find the h1 element in our header and inject the name of
+			// the category into it.
+			$header.find( "h1" ).html( categoryName);
 
-		// Inject the category items markup into the content element.
-		$content.html( markup );
+			// Inject the category items markup into the content element.
+			$content.html( markup );
 
-		// Pages are lazily enhanced. We call page() on the page
-		// element to make sure it is always enhanced before we
-		// attempt to enhance the listview markup we just injected.
-		// Subsequent calls to page() are ignored since a page/widget
-		// can only be enhanced once.
-		$page.page();
+			// Pages are lazily enhanced. We call page() on the page
+			// element to make sure it is always enhanced before we
+			// attempt to enhance the listview markup we just injected.
+			// Subsequent calls to page() are ignored since a page/widget
+			// can only be enhanced once.
+			$page.page();
 
-		// Enhance the listview we just injected.
-		$content.find( ":jqmData(role=listview)" ).listview();
+			// Enhance the listview we just injected.
+			$content.find( ":jqmData(role=listview)" ).listview();
 
-		// We don't want the data-url of the page we just modified
-		// to be the url that shows up in the browser's location field,
-		// so set the dataUrl option to the URL for the category
-		// we just loaded.
-		options.dataUrl = urlObj.href;
+			// We don't want the data-url of the page we just modified
+			// to be the url that shows up in the browser's location field,
+			// so set the dataUrl option to the URL for the category
+			// we just loaded.
+			options.dataUrl = urlObj.href;
 
-		// Now call changePage() and tell it to switch to
-		// the page we just modified.
-		$.mobile.changePage( $page, options );
-	}
+			// Now call changePage() and tell it to switch to
+			// the page we just modified.
+			$.mobile.changePage( $page, options );
+		}
 }
 
 
 // Listen for any attempts to call changePage().
 $(document).bind( "pagebeforechange", function( e, data ) {
 	// We only want to handle changePage() calls where the caller is
+	console.log("got here 1");
 	// asking us to load a page by URL.
 	if ( typeof data.toPage === "string" ) {
 		// We are being asked to load a page by URL, but we only
@@ -88,6 +89,7 @@ $(document).bind( "pagebeforechange", function( e, data ) {
 			// We're being asked to display the items for a specific category.
 			// Call our internal method that builds the content for the category
 			// on the fly based on our in-memory category data structure.
+			console.log("got here 2");
 			showCategory( u, data.options );
 
 			// Make sure to tell changePage() we've handled this call so it doesn't
@@ -96,3 +98,8 @@ $(document).bind( "pagebeforechange", function( e, data ) {
 		}
 	}
 });
+
+/*for(n in json){
+	console.log(n);
+	console.log(json[n].jobType[1]);
+}*/
