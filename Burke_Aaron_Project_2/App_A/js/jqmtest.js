7df5360945f5d954866b33846d3fb68c65789e48
adburke <adburke@fullsail.edu>
2012-09-30 23:42:08 -0500

    JQM refresh fix

--- Burke_Aaron_Project_2/App_A/js/jqmtest.js
+++ Burke_Aaron_Project_2/App_A/js/jqmtest.js
@@ -3,9 +3,7 @@
 // category, inject it into an embedded page, and then make
 // that page the current active page.
 function showCategory( urlObj, options ) {
-	console.log("got here 3");
 	var categoryName = urlObj.hash.replace( /.*category=/, "" );
-		console.log(categoryName);
 		// Get the object that represents the category we
 		// are interested in. Note, that at this point we could
 		// instead fire off an ajax request to fetch the data, but
@@ -18,67 +16,55 @@ function showCategory( urlObj, options ) {
 		var pageSelector = urlObj.hash.replace( /\?.*$/, "" );
 	if (categoryName){
 			// Get the page we are going to dump our content into.
-			var $page = $( pageSelector ),
+		var $page = $( pageSelector ),
 
-				// Get the header for the page.
-				$header = $page.children( ":jqmData(role=header)" ),
+			// Get the header for the page.
+			$header = $page.children( ":jqmData(role=header)" ),
 
-				// Get the content area element for the page.
-				$content = $page.children( ":jqmData(role=content)" );
-				console.log(categoryName);
+			// Get the content area element for the page.
+			$content = $page.children( ":jqmData(role=content)" ),
 
-				// The markup we are going to inject into the content
-				// area of the page.
-				<li data-role="list-divider">Browse By Job Type:</li>
-				var markup = "<ul data-role='listview' data-inset='true'><li data-role='list-divider'>" + categoryName + " Jobs" + "</li>";
-				for(n in json){
-				if ( categoryName === json[n].jobType[1] ) {
-				// The array of items for this category.
-				//cItems = category.items,
-
-				// The number of items in the category.
-				//numItems = cItems.length;
-
-			// Generate a list item f
-				markup += "<li>" + "#: " + json[n].jobNum[1] + "</li>";
+			collapseSet = "<div id='jobs' data-role='collapsible-set'>",
+			markup = "";
+		for(n in json){
+			if ( categoryName === json[n].jobType[1] ) {
+				markup += "<div data-role='collapsible'><h3>" + "#: " + json[n].jobNum[1] + "</h3></div>";
 			}
 		}
-			markup += "</ul>";
+		markup +="</div>";
+		console.log(collapseSet + markup);
+		// Find the h1 element in our header and inject the name of
+		// the category into it.
+		$header.find( "h1" ).html( categoryName);
 
-			// Find the h1 element in our header and inject the name of
-			// the category into it.
-			$header.find( "h1" ).html( categoryName);
+		// Inject the category items markup into the content element.
+		$content.html( collapseSet + markup );
+		// Pages are lazily enhanced. We call page() on the page
+		// element to make sure it is always enhanced before we
+		// attempt to enhance the listview markup we just injected.
+		// Subsequent calls to page() are ignored since a page/widget
+		// can only be enhanced once.
+		$page.page();
 
-			// Inject the category items markup into the content element.
-			$content.html( markup );
+		// Enhance what we just injected.
+		$content.find( ":jqmData(role=collapsible-set)" ).collapsibleset();
 
-			// Pages are lazily enhanced. We call page() on the page
-			// element to make sure it is always enhanced before we
-			// attempt to enhance the listview markup we just injected.
-			// Subsequent calls to page() are ignored since a page/widget
-			// can only be enhanced once.
-			$page.page();
+		// We don't want the data-url of the page we just modified
+		// to be the url that shows up in the browser's location field,
+		// so set the dataUrl option to the URL for the category
+		// we just loaded.
+		options.dataUrl = urlObj.href;
 
-			// Enhance the listview we just injected.
-			$content.find( ":jqmData(role=listview)" ).listview();
-
-			// We don't want the data-url of the page we just modified
-			// to be the url that shows up in the browser's location field,
-			// so set the dataUrl option to the URL for the category
-			// we just loaded.
-			options.dataUrl = urlObj.href;
-
-			// Now call changePage() and tell it to switch to
-			// the page we just modified.
-			$.mobile.changePage( $page, options );
-		}
+		// Now call changePage() and tell it to switch to
+		// the page we just modified.
+		$.mobile.changePage( $page, options );
+	}
 }
 
 
 // Listen for any attempts to call changePage().
 $(document).bind( "pagebeforechange", function( e, data ) {
 	// We only want to handle changePage() calls where the caller is
-	console.log("got here 1");
 	// asking us to load a page by URL.
 	if ( typeof data.toPage === "string" ) {
 		// We are being asked to load a page by URL, but we only
@@ -90,7 +76,6 @@ $(document).bind( "pagebeforechange", function( e, data ) {
 			// We're being asked to display the items for a specific category.
 			// Call our internal method that builds the content for the category
 			// on the fly based on our in-memory category data structure.
-			console.log("got here 2");
 			showCategory( u, data.options );
 
 			// Make sure to tell changePage() we've handled this call so it doesn't
