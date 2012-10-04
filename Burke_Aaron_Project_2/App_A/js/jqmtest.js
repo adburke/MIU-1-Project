51c6e23e701cf303fd2ffd02fb67dc7f362791de
adburke <adburke@fullsail.edu>
2012-10-04 17:59:21 -0500

    Update

--- Burke_Aaron_Project_2/App_A/js/jqmtest.js
+++ Burke_Aaron_Project_2/App_A/js/jqmtest.js
@@ -1,24 +1,63 @@
 
 $('#home').on('pageinit', function(){
-	//code needed for home page goes here
+	
 });
 		
-// $('#addItem').on('pageinit', function(){
+$('#addItem').on('pageshow', function(){
+	// Enables validator debug messages. Used to test the rules: I created.
+	// jQuery.validator.setDefaults({
+	// 	debug: true,
+	// 	success: "valid"
+	// });;
+	var jobNumCount;
+	var myForm = $('#jobForm'),
+		errorsLink = $("#errorsLink");
+	var validator = myForm.validate({
+		rules: {
+			"select-choice-min" : {
+				required: true,
+			},
+			custom: {
+				required: function(element) {
+					return ($("#select-choice-min").val() === "Custom");
+				}
+			}
+		},
+		invalidHandler: function(form, validator) {
+			errorsLink.click();
 
-// 		var myForm = $('#formId');
-// 			myForm.validate({
-// 			invalidHandler: function(form, validator) {
-// 			},
-// 			submitHandler: function() {
-// 		var data = myForm.serializeArray();
-// 			storeData(data);
-// 		}
-// 	});
+			var html = "";
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not("[generated]");
+					//console.log(label.text());
+				var legend = label.closest("fieldset").find(".ui-controlgroup-label");
+				var fieldName = legend.length ? legend.text() : label.text();
+				html += "<li>" + fieldName + "</li>";
+			};
+			$("#formErrors ul").html(html);
+		},
+		submitHandler: function(form) {
+			// var data = myForm.serializeArray();
+			// console.log(data);
+			storeData();
+			form.reset();
+			jobCount();
+		}
+	});
+	// Have reset button clear red validation messages from form created var validator
+	// above to use for this
+	$(".reset").click(function() {
+		//validator.resetForm();
+	});
 	
-// 	//any other code needed for addItem page goes here
-	
-// });
+	//any other code needed for addItem page goes here
+	jobCount();
+	$("#clearData").click(function() {
+		clearLocal();
+	});
+});
 
+// Listen for the page change events by the Browse By buttons on index.html 
 $(document).on( "pagebeforechange", function( e, data ) {
 	// We only want to handle changePage() calls where the caller is
 	// asking us to load a page by URL.
@@ -42,53 +81,45 @@ $(document).on( "pagebeforechange", function( e, data ) {
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+// Function that populates the Browse By category place holder page in index.html
 function showCategory( urlObj, options ) {
-	var categoryName = urlObj.hash.replace( /.*category=/, "" );
-	var pageSelector = urlObj.hash.replace( /\?.*$/, "" );
-	
-	if (categoryName != "displayAll"){
-			// Get the page we are going to dump our content into.
-		var $page = $( pageSelector ),
+	var categoryName = urlObj.hash.replace( /.*category=/, "" ),
+		pageSelector = urlObj.hash.replace( /\?.*$/, "" ),
+		// Get the page we are going to dump our content into
+		$page = $( pageSelector ),
 
-			// Get the header for the page.
-			$header = $page.children( ":jqmData(role=header)" ),
+		// Get the header for the page.
+		$header = $page.children( ":jqmData(role=header)" ),
 
-			// Get the content area element for the page.
-			$content = $page.children( ":jqmData(role=content)" ),
+		// Get the content area element for the page.
+		$content = $page.children( ":jqmData(role=content)" ),
 
-			collapseSet = "<div id='jobs' data-role='collapsible-set' data-content-theme='b'>",
-			markup = "";
-		for(n in json){
+		collapseSet = "<div id='jobs' data-role='collapsible-set' data-content-theme='b'>",
+		markup = "";
+	if (categoryName != "displayAll" && categoryName != "jobInfo"){
+		// For each category selected
+		for(var n in json){
 			if ( categoryName === json[n].jobType[1] ) {
 				var object = json[n];
 				markup += "<div data-role='collapsible' data-inset='true'><h3>" + "#: " + json[n].jobNum[1] + "</h3><ul data-role='listview' data-inset='true'>";
-				for (x in object){
+				for (var x in object){
 					markup += "<li>" + object[x][0] + ": " +object[x][1] + "</li>";
 				};
-				markup += "</div>"
+				markup += "</ul></div>";
 			};
 		};
 		markup +="</div></ul>";
 		// Find the h1 element in our header and inject the name of the category into it.
 		$header.find( "h1" ).html( categoryName);
 	} else if ( categoryName === "displayAll"){
-		var $page = $( pageSelector ),
-
-			// Get the header for the page.
-			$header = $page.children( ":jqmData(role=header)" ),
-
-			// Get the content area element for the page.
-			$content = $page.children( ":jqmData(role=content)" ),
-
-			collapseSet = "<div id='jobs' data-role='collapsible-set' data-content-theme='b'>",
-			markup = "";
-		for(n in json){
+		for(var n in json){
 			var object = json[n];
 			markup += "<div data-role='collapsible' data-inset='true'><h3>" + "#: " + json[n].jobNum[1] + "</h3><ul data-role='listview' data-inset='true'>";
-			for (x in object){
+			for (var x in object){
 				markup += "<li>" + object[x][0] + ": " +object[x][1] + "</li>";
 			};
-			markup += "</div>"
+			markup += "</ul></div>";
 		};
 		markup +="</div></ul>";
 		// Find the h1 element in our header and inject the name of the category into it.
@@ -129,16 +160,73 @@ var getData = function(){
 
 };
 
-var storeData = function(data){
-	
+function jobCount(){
+	if (localStorage.getItem("jobNumber")){
+		jobNumCount = localStorage["jobNumber"];
+		$("#jobnum").val(Number(jobNumCount));
+	} else {
+		jobNumCount = 1000;
+		localStorage.setItem("jobNumber", jobNumCount.toString());
+		$("#jobnum").val(jobNumCount);
+	};
+	//console.log(jobNumCount);
+};
+
+var storeData = function(key){
+	// Random key number for each job object
+	// Check to see if we are editing an existing item or it is a new item.
+	if (!key || key === undefined){
+		var id = jobNumCount;
+		var num = Number($("#jobnum").val())+1;
+		localStorage["jobNumber"] = num.toString();
+	} else {
+		id = key;
+		
+	};
+	// Get Radio button status
+	// getSelectedRadio();
+	// Get all of the form data and create an object out of it
+	var jobFormData				= {};
+		jobFormData.jobNum		= ["Job Num", $("#jobnum").val()];
+		jobFormData.company		= ["Company", $("#company").val()];
+		jobFormData.address		= ["Address", $("#address").val()];
+		jobFormData.city		= ["City", $("#city").val()];
+		jobFormData.state		= ["State", $("#state").val()];
+		jobFormData.zipcode		= ["Zipcode", $("#zipcode").val()];
+		jobFormData.phone		= ["Phone", $("#phone").val()];
+		jobFormData.email		= ["Email", $("#email").val()];
+		jobFormData.oDate		= ["Order Date", $("#orderdate").val()];
+		jobFormData.needDate	= ["Need Date", $("#needbydate").val()];
+		jobFormData.rushOrder	= ["Rush Order", $('input:radio[name=rush]:checked').val()];
+		jobFormData.jobType		= ["Job Type", $("#select.jobTypeList").val()];
+		jobFormData.customInfo	= ["Custom Info", $("#custom").val()];
+		jobFormData.quantity	= ["Quantity", $("#qty").val()];
+		jobFormData.prodHours	= ["Production Hours", $("#production").val()];
+		jobFormData.designEff	= ["Design Effort", $("#design").val()];
+
+	localStorage.setItem(id, JSON.stringify(jobFormData));
+	if (!key || key === undefined){
+	alert("Job #: " + jobNumCount + " Saved");
+	} else {
+		alert("Job #: " + key + " Saved");
+	};
+	jobCount();	
 };
 
 var	deleteItem = function (){
-			
+	var ask = confirm("Are you sure you want to delete this job?");
+	if(ask){
+		localStorage.removeItem(this.key);
+		alert("Job was deleted!");
+		window.location.reload();
+	} else{
+		alert("Job was NOT deleted.");
+	};
 };
+
 					
 var clearLocal = function(){
-
+	localStorage.clear();
+	alert("All jobs deleted from local storage.");
 };
-
 
